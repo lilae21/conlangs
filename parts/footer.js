1597ccb256cdb8cfe019906c7e88a084436dafc3
lilaë <elivec21@gmail.com>
2025-03-30 16:28:30 +0200

    Update footer.js

--- parts/footer.js
+++ parts/footer.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("footer.htm")
+    fetch("https://lilae21.github.io/conlangs/parts/footer.htm")
         .then(response => response.text())
         .then(html => {
             // Aggiunge il footer al fondo della pagina
             document.body.insertAdjacentHTML("beforeend", html);
 
             // Dopo aver caricato il footer, carica il JSON e aggiorna l'anno
-            fetch("footer-config.json")
+            fetch("https://lilae21.github.io/conlangs/parts/footer-config.json")
                 .then(response => response.json())
                 .then(config => {
                     const footerYear = document.getElementById("footer-year");
