62f529f95f2dd3b736fc562dc839c0425a079e6c
lilaë <elivec21@gmail.com>
2025-03-30 16:23:41 +0200

    Add files via upload

--- parts/footer.js
+++ parts/footer.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("./parts/footer.htm")
+    fetch("footer.htm")
         .then(response => response.text())
         .then(html => {
             // Aggiunge il footer al fondo della pagina
             document.body.insertAdjacentHTML("beforeend", html);
 
             // Dopo aver caricato il footer, carica il JSON e aggiorna l'anno
-            fetch("./parts/footer-config.json")
+            fetch("footer-config.json")
                 .then(response => response.json())
                 .then(config => {
                     const footerYear = document.getElementById("footer-year");
